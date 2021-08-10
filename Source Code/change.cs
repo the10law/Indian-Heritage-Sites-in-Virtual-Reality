46ca20a44b6bbec81de483a3b36f15f91f9d4313
Rahul Kumar Singh <69062880+the10law@users.noreply.github.com>
2021-08-10 12:25:35 +0530

    Add files via upload

---
+++ Source Code/change.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class change : MonoBehaviour
+{
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        Invoke("delayback", 5);
+    }
+    public void delayback()
+    {
+        
+            SceneManager.LoadScene("sites");
+
+    }
+}
